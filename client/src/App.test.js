b9b34972e3174a4d4089d4c4b30a3d7ab0229a3b
Danny Manzietti <danmanzietti@gmail.com>
2019-10-04 13:57:20 -0400

    finished MVP. Two hooks, dark mode and useLocalStorage Working on styling

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,23 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
 import { render } from "@testing-library/react";
 
 import App from "./App";
-import PlayerCard from "./components/PlayerCard";
 
-test("button renders without crashing", () => {
-  render(<Button />);
-});
-
-test('contains "toggle dark mode" button', () => {
-  const { getByText } = render(<Button />);
-
-  getByText(/toggle dark mode/i);
-});
-
-test("woman soccer player data renders on website", () => {
-  const { getByText } = render(<h1>woman soccer player data</h1>);
-
-  getByText(/woman soccer player data/i);
+it("app renders without crashing", () => {
+  render(<App />);
 });
